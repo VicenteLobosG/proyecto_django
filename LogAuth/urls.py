8f0536d15d0df03b5b86c44a103ca5068051075e
Miguel <stuvms@gmail.com>
2020-06-20 23:11:12 -0400

    urls

--- LogAuth/urls.py
+++ LogAuth/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from LogAuth import views
 
+
+
+app_name='auth'
+
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
