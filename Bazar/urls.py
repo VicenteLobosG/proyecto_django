acb27ce7fa1f8b24c3ec6d86fdd2cab1240352f8
Miguel <stuvms@gmail.com>
2020-06-21 21:36:08 -0400

    config imagenes admin

--- Bazar/urls.py
+++ Bazar/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
@@ -9,3 +12,6 @@ urlpatterns = [
     path('app1/', include('app1.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
